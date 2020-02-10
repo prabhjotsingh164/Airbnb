68382ed9e9f20e4c470d032933f247b09f880a92
prabhjotsingh164 <mig1994.prabh@gmail.com>
2020-02-10 14:24:08 -0500

    second commit

--- public/js/exp.js
+++ public/js/exp.js
@@ -4,6 +4,27 @@ var modal = document.getElementById('id01');
 var modal = document.getElementById('id02');
 
 
+function validateForm() {
+    var x = document.forms["myForm"]["uname"].value;
+    var x1 = document.getElementById("psw").value;
+    
+    if (x == "" ) {
+      alert("Name must be filled out");
+      return false;
+    }
+    if(x1 =="")
+    {
+        alert("Password must be filled out");
+        return false;
+    }
+    if(x1.length<8)
+    {
+        alert("Password must be atleast 8 characters");
+        return false;
+
+    }
+    
+  }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
