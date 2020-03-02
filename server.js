1cb9cefe80a5aef894f38a670f6849585dc0621b
prabhjotsingh164 <mig1994.prabh@gmail.com>
2020-03-02 09:15:24 -0500

    email and sms commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const app = express();
+
 
 //This allows express to make staic content available from the public
 app.use(express.static('public'))
@@ -24,63 +26,171 @@ app.get("/room-listing",(req,res)=>{
         
     })
 });
+app.get("/signup",(req,res)=>{
+    res.render("signup",{
+        title:"Signup",
+        headinginfo: "Signup",
+        
+    })
+});
+app.get("/login",(req,res)=>{
+    res.render("login",{
+        title:"Login",
+        headinginfo: "Login",
+        
+    })
+});
+app.get("/welcome",(req,res)=>{
+    res.render("welcome",{
+        title:"Welcome",
+        headinginfo: "Welcome",
+        
+    })
+});
 // app.get("/sendMessage",(req,res)=>{
 //     res.render("form",{
 //         title:"SMS Page"
 //     });
     
 // });
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'mig1994.prabh@gmail.com',
+      pass: '1994fengles!'
+    }
+  });
+app.post("/login",(req,res)=>{
+    const errors= [];
+   
+
+  if(req.body.uname=="")
+  {
+    errors.push("Sorry, you must enter your Username")
+  }
+  if(req.body.psw=="")
+  {
+      errors.push("enter your password")
+  }
 
 
-// app.post("",(req,res)=>{
-//     const errors= [];
-//     if(req.body.phoneNo=="")
-//   {
-//     errors.push("Sorry, you must enter a phone number");
+  if(errors.length > 0)
+  {
+    res.render("login",{
+      messages : errors
+    })
+  }
+  else
+  {
+      
+    const accountSid = 'AC19c80835ab35434b88c649ae6e26b9a1';
+    const authToken = 'c993064fd7e8dff47cd51401cef4373b';
+    const client = require('twilio')(accountSid, authToken);
+    const mailOptions = {
+        from: 'mig1994.prabh@gmail.com',
+        to: 'mig1994.prabh@gmail.com',
+        subject: 'Welcome to Airbnb',
+        text: 'Welcome to Airbnb. Hope you find the best room you are looking for. Enjoy'
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
 
-//   }
-
-//   if(req.body.message=="")
-//   {
-//     errors.push("Sorry, youmust enter a  message")
-//   }
-//   if(req.body.uname=="")
-//   {
-//       errors.push("enter the username")
-//   }
-
-
-//   if(errors.length > 0)
-//   {
-//     res.render("form",{
-//       messages : errors
-//     })
-//   }
-//   else
-//   {
-//     const accountSid = 'PUT YOUR ACCOUNT SID HERE';
-//     const authToken = 'PUT HERE YOUR AUTHTOKEN HERE';
-//     const client = require('twilio')(accountSid, authToken);
     
-//     client.messages
-//       .create({
-//          body: `${req.body.firstName} ${req.body.lastName} Message :${req.body.message}`,
-//          from: 'PUT YOUR TRIAL NUMBER HERE',
-//          to: `${req.body.phoneNo}`
-//        })
-//       .then(message => {
-//         console.log(message.sid);
-//         res.render("home");
-//       })
-//       .catch((err)=>{
-//           console.log(`Error ${err}`);
-//       })
-
-//   }
+    client.messages
+      .create({
+         body: `Welcome ${req.body.first_name} ${req.body.last_name} back to Airbnb. Rooms at your fingertips`,
+         from: '+12078020196',
+         to: `+14373881668`
+       })
+      .then(message => {
+        console.log(message.sid);
+        res.render("welcome");
+      })
+      .catch((err)=>{
+          console.log(`Error ${err}`);
+      })
+      
+  }
     
 
 
-// });
+});
+
+
+
+
+
+
+
+app.post("/signup",(req,res)=>{
+    const errors= [];
+    if(req.body.email=="")
+  {
+    errors.push("Sorry, you must enter an email address");
+
+  }
+
+  if(req.body.first_name=="")
+  {
+    errors.push("Sorry, you must enter a  first name")
+  }
+  if(req.body.last_name=="")
+  {
+      errors.push("enter the last name")
+  }
+
+
+  if(errors.length > 0)
+  {
+    res.render("signup",{
+      messages : errors
+    })
+  }
+  else
+  {
+    const accountSid = 'AC19c80835ab35434b88c649ae6e26b9a1';
+    const authToken = 'c993064fd7e8dff47cd51401cef4373b';
+    const client = require('twilio')(accountSid, authToken);
+
+    const mailOptions = {
+        from: 'mig1994.prabh@gmail.com',
+        to: 'mig1994.prabh@gmail.com',
+        subject: 'Welcome to Airbnb',
+        text: 'Welcome to Airbnb. Hope you find the best room you are looking for. Enjoy'
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    client.messages
+      .create({
+         body: `Welcome ${req.body.first_name} ${req.body.last_name} to Airbnb. Hope you find what you are looking for.`,
+         from: '+12078020196',
+         to: `+14373881668`
+       })
+      .then(message => {
+        console.log(message.sid);
+        res.render("welcome");
+      })
+      .catch((err)=>{
+          console.log(`Error ${err}`);
+      })
+
+  }
+    
+
+
+});
 const PORT = 3000;
 app.listen(3000,()=>{
 console.log(`web serve is up and running`);
