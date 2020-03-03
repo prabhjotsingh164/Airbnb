2958ab67dec63a404ee1ea88f936780270c65a6c
prabhjotsingh164 <mig1994.prabh@gmail.com>
2020-03-02 20:03:08 -0500

    models commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const productModel = require("./models/products");
 const app = express();
 
 
