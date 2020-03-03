3944939b3e3a4ab7866c09e8972340feb2f32839
prabhjotsingh164 <mig1994.prabh@gmail.com>
2020-03-02 20:30:14 -0500

    controllers commit

--- controllers/general.js
+++ controllers/general.js
@@ -1,2 +1,40 @@
 const express =require('express')
 const router = express.Router();
+router.get("/",(req,res)=>{
+    res.render("index",{
+        title:"Home",
+        headinginfo: "Home Page",
+        randomContent:"AAAAAAAAAAAAA"
+    })
+    });
+    
+    router.get("/room-listing",(req,res)=>{
+        res.render("roomlisting",{
+            title:"Room Listings",
+            headinginfo: "Room Listing",
+            
+        })
+    });
+    router.get("/signup",(req,res)=>{
+        res.render("signup",{
+            title:"Signup",
+            headinginfo: "Signup",
+            
+        })
+    });
+    router.get("/login",(req,res)=>{
+        res.render("login",{
+            title:"Login",
+            headinginfo: "Login",
+            
+        })
+    });
+    router.get("/welcome",(req,res)=>{
+        res.render("welcome",{
+            title:"Welcome",
+            headinginfo: "Welcome",
+            
+        })
+    });
+
+    module.exports=router;
